a1e5ed8a7f6626ac858ad2dbf2a3f5144b2a32cf
Anna Gumna <148956955+AnnaG02@users.noreply.github.com>
2024-01-29 22:44:09 +0100

    Add files via upload

--- drone_controller/drone_controller/drone_project.py
+++ drone_controller/drone_controller/drone_project.py
@@ -1,13 +1,11 @@
 import rclpy
 from rclpy.node import Node
-
 from geometry_msgs.msg import Twist, Pose
-
 
 class DroneController(Node):
     def __init__(self):
         super().__init__('drone_controller')
-        
+
         # Current pose subscriber
         self.gt_pose_sub = self.create_subscription(
             Pose,
@@ -19,38 +17,58 @@ class DroneController(Node):
 
         # Control command publisher
         self.command_pub = self.create_publisher(Twist, '/drone/cmd_vel', 10)
-        
-        # Callback for executing a control commands
+
+        # Parameters for square path
+        self.square_side_length = 2.0  
+        self.linear_speed = 0.2  
+        self.angular_speed = 0.5  
+
+        # Callback for executing control commands
         timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
-        # Feel fre to fill with your code! Add some objects to represent a goal points to achieve
+        # Initialize variables for square path
+        self.target_pose = Pose()
+        self.target_pose.position.x = 0.0
+        self.target_pose.position.y = 0.0
+        self.target_pose.orientation.z = 0.0
+        self.state = "move_forward"
 
-    
     def pose_callback(self, data):
         self.gt_pose = data
-        print(f"{data}")
-
-    
-    def timer_callback(self):
-        # HINT: Check a current pose. Use it to check if a drone achieved a desired pose.
         print(f"Current pose: {self.gt_pose}")
-        
-        # HINT: Use a self.command_pub to publish a command
-        # Fill with your code!
-        print("Published!")
 
+    def timer_callback(self):
+        if self.gt_pose is not None:
+            if self.state == "move_forward":
+                
+                if self.gt_pose.position.x < self.target_pose.position.x + self.square_side_length:
+                    cmd_vel = Twist()
+                    cmd_vel.linear.x = self.linear_speed
+                    self.command_pub.publish(cmd_vel)
+                else:
+                    
+                    self.state = "turn_right"
+                    self.target_pose.orientation.z += 90.0  # 90 degrees
 
+            elif self.state == "turn_right":
+              
+                if self.gt_pose.orientation.z < self.target_pose.orientation.z:
+                    cmd_vel = Twist()
+                    cmd_vel.angular.z = self.angular_speed
+                    self.command_pub.publish(cmd_vel)
+                else:
+                   
+                    self.state = "move_forward"
+                    self.target_pose.position.x = self.gt_pose.position.x
 
 def main(args=None):
     rclpy.init(args=args)
-
     node = DroneController()
-
     rclpy.spin(node)
-
     node.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
+
